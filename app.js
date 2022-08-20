5b4245f676ba357ab229b42ca12d8d93509a4687
zhuochen <zhuochen777@gmail.com>
2022-08-19 17:13:19 -0700

    use passport.js local to add cookies for user register

--- app.js
+++ app.js
@@ -4,13 +4,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+//set up session using session package
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+//initialize passport
+app.use(passport.initialize());
+
+//use passport to set up session
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
@@ -19,12 +34,16 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-//add plugin to schema before creating mongoose model
-//use secret long string to encrypt our database
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});//access SECRET variable in .env
-//add encrypt as a plugin to schema and pass in secret long string and only encrypt password field
+//add plugin to mongoose schema, use passport local mongoose to hash and salt passwords and save users into mongodb db
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
+
+//Passport/Passport-Local Configuration, setup passport-local LocalStrategy
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //to do
 app.get("/", function(req, res) {
@@ -39,39 +58,37 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 
+app.get("/secrets", isLoggedIn, function(req, res) {
+    res.render("secrets");
+});
+
 
 app.post("/register", function(req, res) {
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password
-    });
-
-    user.save(function(err) {
-        if (!err) {
-            res.render("secrets");
-        } else {
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+        if (err) {
             console.log(err);
-        }
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
+        };
     });
 
 });
 
 app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    User.findOne({username: username}, function(err, foundUser) {
-        if (!err) {
-            if (foundUser.password === password) {
-                res.render("secrets");
-            } else {
-                res.send("no matching username and password found!");
-            }
-        } else {
-            console.log(err);
-        }
-    });
 });
+
+// check isLoggedIn
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 
 app.listen(3000, function() {
     console.log("server started on port 3000");
